05f31ade7b3612a73497b5c101592691b965298e
S0ul <kolegabezd@gmail.com>
2025-09-19 22:20:07 +0200

    update

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.GuildMessages
     ]
 });
 
