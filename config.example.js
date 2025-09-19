386d066e846a23d5c9e9ff364e5e7b24e2a8eecf
S0ul <kolegabezd@gmail.com>
2025-09-19 22:16:59 +0200

    Add clean config.example.js and update bot to use environment variables

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// Copy this file to config.js and add your bot token
+module.exports = {
+    // Your Discord bot token
+    // Get this from https://discord.com/developers/applications
+    DISCORD_TOKEN: 'your_bot_token_here'
+};
